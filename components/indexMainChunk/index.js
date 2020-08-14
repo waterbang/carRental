f7957d2266a7ad9b0c9efc8db96bfd985c1a8603
waterbang <409071692@qq.com>
2020-08-14 14:30:23 +0800

    new index header for add

---
+++ components/indexMainChunk/index.js
@@ -0,0 +1,26 @@
+// components/indexMainChunk/index.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    flag: {
+      type: String,
+      value: 'left'
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
